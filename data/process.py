d553e4940a83e8fef528925069ae83d75c1483a9
AsherHoltham <asher.holtham.github@gmail.com>
2024-10-31 00:00:16 -0700

    data parsing testing trials

--- data/process.py
+++ data/process.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import math
+import os
 
 import processing as p
 
@@ -15,10 +17,16 @@ validation_len =  math.floor(set_len * .15)
 test_len = set_len - validation_len - train_len
 
 df.set_index(df.columns[1], inplace=True)
-for f in df:
-    data = p.converter(f)
+for f in df.index:
+    filename = str(f)
+    filename = filename[:-4]
     l = int(df.loc[f])
+    l = str(l)
 
-    if(df.loc(f) < train_len): p.allocater(data, f, l, 'train')
-    elif(df.loc(f) < train_len + validation_len): p.allocater(data, f, l, 'validation')
-    else: p.allocater(data, f, l, 'test')
+    file = l + filename + '.npy'
+    np.save(file,  p.converter('/Users/asheraugustusholtham/Desktop/data/wavfiles/' + f))
+
+    if(df.loc(f) < train_len): os.path.join('train', file)
+    elif(df.loc(f) < train_len + validation_len): os.path.join('validation', file)
+    else: os.path.join('test', file)
+    
--- data/processing/data_allocator.py
+++
@@ -1,7 +0,0 @@
-import os
-import numpy as np
-
-def allocater(input, file, label, odir):
-    new_data = str(label + file) + '.npy'
-    np.save(new_data,  input)
-    os.path.join(odir, new_data)
