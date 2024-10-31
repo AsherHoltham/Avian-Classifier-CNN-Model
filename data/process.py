4564e4965109cd6696bb6fbc4d8a89763091c285
AsherHoltham <asher.holtham.github@gmail.com>
2024-10-30 22:53:01 -0700

    parsing close to finished

--- data/process.py
+++ data/process.py
@@ -8,13 +8,14 @@ p.parser("/Users/asheraugustusholtham/Desktop/data/bird_songs_metadata.csv")
 
 df = pd.read_csv('data.csv')
 
+# Define sizes of each dataset for training the model
 set_len = len(df)
 train_len =  math.floor(set_len * .7)
 validation_len =  math.floor(set_len * .15)
 test_len = set_len - validation_len - train_len
 
-print(train_len)
-print(validation_len)
-print(test_len)
-
-print(set_len)
+df.set_index(df.columns[1], inplace=True)
+for f in df:
+    if(df.loc(f) < train_len): p.converter(f, 'train')
+    elif(df.loc(f) < train_len + validation_len): p.converter(f, 'validation')
+    else: p.converter(f, 'test_data'): p.converter(f, 'test')
