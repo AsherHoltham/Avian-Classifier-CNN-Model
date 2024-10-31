44409a45000ff9b2554cb43af293a8b95380ae9e
AsherHoltham <asher.holtham.github@gmail.com>
2024-10-30 20:12:43 -0700

    finish config.py file for managing os files and also added .sh script to run on VM

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:
+    def __init__(self):
+        self.local_path = os.path.dirname(os.path.abspath(__file__))
+
+        #Data file path tracking
+        self.data_path = os.path.join(self.local_path, 'data')
+        self.train_data_path = os.path.join(self.data_path, 'train_data')
+        self.test_data_path = os.path.join(self.data_path, 'test_data')
+
+        # Output path tracking
+        self.output_path = os.path.join(self.local_path, 'outputs')
+        self.model_save_path = os.path.join(self.output_path, 'models')
+        self.log_path = os.path.join(self.output_path, 'logs')
+
+
+
+
+        self.learning_rate = 1e-3
+        self.batch_size = 16
+        self.epochs = 10
---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as numpy
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
